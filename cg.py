680bd9b56272231808f9b441d4622843689985fb
Enoch Y. Chau <eychau1@uci.edu>
2020-03-31 11:47:18 -0700

    callback buttons

--- cg.py
+++ cg.py
@@ -6,6 +6,59 @@ import RPi.GPIO as GPIO
 from hx711 import HX711
 import I2C_LCD_driver
 
+BX_on_off = 19
+BX_tare = 13
+BX_timer = 26
+BX_coffee_g = 20
+BX_water_g = 21
+
+IN_on_off=False
+IN_timer=False
+IN_tare=False
+IN_coffee_g=False
+IN_water_g=False
+
+#on off vars
+onoff_state = False
+
+#timer vars
+start_time = 0
+brew_time = 0
+start_lock = False
+
+#weight vars
+coffee_g = 0
+water_g = 0
+
+def CB_on_off (BX_on_off):
+    global IN_on_off
+    if args.verbose:
+        print("BX_on_off pressed")
+    IN_on_off=True
+
+def CB_tare(BX_tare) :
+    global IN_tare
+    if args.verbose:
+        print("BX_tare pressed")
+    IN_tare = True
+
+def CB_timer(BX_timer):
+    global IN_timer
+    if args.verbose:
+        print("BX_timer pressed")
+    IN_timer=True
+
+def CB_coffee_g(BX_coffee_g):
+    global IN_coffee_g
+    if args.verbose:
+        print("BX_coffee_g pressed")
+    IN_coffee_g=True
+
+def CB_water_g(BX_water_g):
+    global IN_water_g
+    if args.verbose:
+        print("BX_water_g pressed")
+    IN_water_g=True
 
 if __name__ == "__main__":
     #arge parser
@@ -19,23 +72,22 @@ if __name__ == "__main__":
         tool_type="melittaPourOver"
     elif args.tool=='fp' :
         tool_type="frenchpress"
-    else :
-        tool_type=""
+    else:
+        print("invalid tool type")
+        sys.exit()
+
 
     #set up the pins
-    BX_on_off = 19
-    BX_tare = 13
-    BX_timer = 26
-    BX_coffee_g = 20
-    BX_water_g = 21
-
-    BX_list = [BX_tare, BX_on_off, BX_timer, BX_coffee_g, BX_water_g]
-
+    BX_list = [BX_tare,BX_on_off,BX_timer,BX_coffee_g,BX_water_g]
+    CB_list = [CB_tare,CB_on_off,CB_timer,CB_coffee_g,CB_water_g]
     if args.verbose:
         print("setting up buttons")
 
     GPIO.setmode(GPIO.BCM)#basically the GPIO numbers instead of the physical pin numbers
     GPIO.setup(BX_list, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    for CB,BX in zip(CB_list,BX_list) :
+        GPIO.add_event_detect(BX,GPIO.FALLING,callback=CB,bouncetime=200)
 
     if args.verbose:
         print("setting up hx711 & loadcell")
@@ -52,48 +104,28 @@ if __name__ == "__main__":
     mylcd.lcd_clear()
     mylcd.backlight(0)
 
-    #on off vars
-    onoff_state = False
-
-    #timer vars
-    start_time = 0
-    brew_time = 0
-    start_lock = False
-
-    #weight vars
-    coffee_g = 0
-    water_g = 0
-
     if args.verbose:
         print("setup complete")
 
     while True :
         try:
-            IN_on_off = GPIO.input(BX_on_off)
-
-            if not IN_on_off:
+            if IN_on_off:
                 onoff_state = not onoff_state
                 mylcd.lcd_clear()
-                if onoff_state == True :
+                if onoff_state : #system is on
                     mylcd.backlight(1)
                     mylcd.lcd_display_string("Welcome to",1)
                     mylcd.lcd_display_string("Coffee Grams",2)
-                else :
+                else : #system is off
                     mylcd.backlight(0)
                     coffee_g = 0
                     water_g = 0
                     start_time = 0
                     brew_time = 0
                     start_lock = False
-                time.sleep(0.5)
+                IN_on_off=False
 
             if onoff_state :
-
-                #read button states
-                IN_timer = GPIO.input(BX_timer)
-                IN_tare = GPIO.input(BX_tare)
-                IN_coffee_g = GPIO.input(BX_coffee_g)
-                IN_water_g = GPIO.input(BX_water_g)
 
                 #update weight
                 current_weight = f"{hx.get_weight(5):.2f}"
@@ -104,47 +136,53 @@ if __name__ == "__main__":
                 if args.verbose:
                     print(f"weight: {current_weight}")
 
-                current_time = time.time()
+                current_time = f"{time.strftime('%m-%d-%Y %H:%M:%S', time.localtime(time.time()))}"
                 if start_lock :
-                    current_time = time.time()-start_time-57600 #idk why it kept displaying 16 in the hours position
+                    current_time = f"Timer: {time.strftime('%M:%S', time.localtime(time.time()-start_time-57600))}    "#idk why it kept displaying 16 in the hours position
 
-                mylcd.lcd_display_string(f"{time.strftime('%H:%M:%S', time.localtime(current_time))}             ",1)
+                mylcd.lcd_display_string(current_time,1)
                 if args.verbose:
-                    print(f"{time.strftime('%H:%M:%S', time.localtime(current_time))}")
+                    print(current_time)
 
                 #timer start/stop
-                if not IN_timer:
+                if IN_timer:
                     if start_lock:
                         brew_time = time.strftime('%H:%M:%S', time.localtime(time.time() - start_time))
+                        mylcd.lcd_display_string("Brew time saved ",1)
+                        time.sleep(.5)
                         if args.verbose:
                             print(f"brew time: {brew_time}")
                     else:
                         start_time = time.time()
                     start_lock = not start_lock
-                    time.sleep(0.5)
+                    IN_timer=False
 
                 #tare scale
-                if not IN_tare:
+                if IN_tare:
                     hx.tare()
-                    time.sleep(0.5)
+                    IN_tare=False
+                    tare_str = "Scale Tared"
+                    # mylcd.lcd_display_string(tare_str+"     ",2)
+                    if args.verbose:
+                        print(tare_str)
 
                 #record weights
-                if not IN_coffee_g:
+                if IN_coffee_g:
                     coffee_g = float(current_weight)
                     mylcd.lcd_display_string("coffee pushed   ",2)
-                    time.sleep(0.5)
+                    time.sleep(.5)
                     if args.verbose:
                         print(f"coffee weight recorded: {coffee_g}")
+                    IN_coffee_g=False
 
-                if not IN_water_g:
+
+                if IN_water_g:
                     water_g = float(current_weight)
-
                     mylcd.lcd_display_string("water pushed    ",2)
-                    time.sleep(0.5)
+                    time.sleep(.5)
                     if args.verbose:
                         print(f"water weight recorded: {water_g}")
-
-
+                    IN_water_g=False
 
                 # ID_num = 1
                 # coffee_g = 14.1
