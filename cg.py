63470c689adc3eb086a4fbf7b871a88d5339344b
Enoch Y. Chau <eychau1@uci.edu>
2020-04-05 00:27:33 -0700

    Update cg.py

--- cg.py
+++ cg.py
@@ -3,15 +3,17 @@ import time
 import sys
 import argparse
 import RPi.GPIO as GPIO
-from hx711 import HX711
+from hx711 import HX711 #for raspberry pi 3b+ use pin 17 for 3v3, it seems to have much less noise than pin 1
 import I2C_LCD_driver
 
+#button macros
 BX_on_off = 19
 BX_tare = 13
 BX_timer = 26
 BX_coffee_g = 20
 BX_water_g = 21
 
+#button input states for event and callback
 IN_on_off=False
 IN_timer=False
 IN_tare=False
@@ -30,6 +32,8 @@ start_lock = False
 coffee_g = 0
 water_g = 0
 
+#button callbacks
+#use globals b/c callback is on a different thread
 def CB_on_off (BX_on_off):
     global IN_on_off
     if args.verbose:
@@ -76,8 +80,7 @@ if __name__ == "__main__":
         print("invalid tool type")
         sys.exit()
 
-
-    #set up the pins
+    #set up buttons
     BX_list = [BX_tare,BX_on_off,BX_timer,BX_coffee_g,BX_water_g]
     CB_list = [CB_tare,CB_on_off,CB_timer,CB_coffee_g,CB_water_g]
     if args.verbose:
@@ -89,6 +92,7 @@ if __name__ == "__main__":
     for CB,BX in zip(CB_list,BX_list) :
         GPIO.add_event_detect(BX,GPIO.FALLING,callback=CB,bouncetime=200)
 
+    #set up hx711 & loadcell
     if args.verbose:
         print("setting up hx711 & loadcell")
 
@@ -98,6 +102,7 @@ if __name__ == "__main__":
     hx.reset()
     hx.tare()
 
+    #set up lcd
     if args.verbose:
         print("setting up LCD")
     mylcd = I2C_LCD_driver.lcd()
@@ -113,21 +118,35 @@ if __name__ == "__main__":
                 onoff_state = not onoff_state
                 mylcd.lcd_clear()
                 if onoff_state : #system is on
+                    hx.power_up()
                     mylcd.backlight(1)
                     mylcd.lcd_display_string("Welcome to",1)
                     mylcd.lcd_display_string("Coffee Grams",2)
                 else : #system is off
+                    hx.power_down()
                     mylcd.backlight(0)
+
+                    #reset variables
                     coffee_g = 0
                     water_g = 0
-                    start_time = 0
                     brew_time = 0
+                    start_time = 0
                     start_lock = False
+
                 IN_on_off=False
 
             if onoff_state :
 
                 #update weight
+                # if args.verbose:
+                #     print("collecting weight samples")
+                # current_weight=0
+                # sample_size=4
+                # for i in range(sample_size):
+                #     current_weight+=hx.get_weight(5)
+                # current_weight = current_weight/sample_size
+                # current_weight = f"{current_weight:.2f}"
+
                 current_weight = f"{hx.get_weight(5):.2f}"
 
                 #User Interface
@@ -184,43 +203,48 @@ if __name__ == "__main__":
                         print(f"water weight recorded: {water_g}")
                     IN_water_g=False
 
-                # ID_num = 1
+                ID_num = 1
                 # coffee_g = 14.1
-                # water_g = 222.4
+                water_g = 222.4
                 # brew_time = "00:03:43"
                 #
-                #if water_g and coffee_g and brew_time :
-                    # try :
-                    #     db = mysql.connector.connect(user='coffee_grams_admin', password='well50',
-                    #                                 host='localhost',
-                    #                                 database='coffee_grams')
-                    #     if args.verbose:
-                    #         print("db opened")
-                    #
-                    #     cursor = db.cursor()
-                    #
-                    #     insert_stm = "INSERT INTO " + tool_type  + " (ID, datetime, coffee, water, brew_time, type) VALUES (%s, %s, %s, %s, %s, %s)"
-                    #     data = (ID_num  ,time.strftime('%Y-%m-%d %H:%M:%S') , coffee_g , water_g , brew_time,args.coffee_type)
-                    #
-                    #     cursor.execute(insert_stm,data)
-                    #
-                    #     db.commit()
-                    #
-                    #     if args.verbose:
-                    #         print("data inserted")
-                    #
-                    # except mysql.connector.Error as e :
-                    #     print("Error:",e)
-                    #
-                    # finally :
-                    #     if db.is_connected() :
-                    #         db.close()
-                    #         cursor.close()
-                    #
-                    #         if args.verbose:
-                    #             print("db closed")
+                if water_g and coffee_g and brew_time :
+                    try :
+                        db = mysql.connector.connect(user='coffee_grams_admin', password='well50',
+                                                    host='localhost',
+                                                    database='coffee_grams')
+                        if args.verbose:
+                            print("db opened")
+
+                        cursor = db.cursor()
+
+                        insert_stm = "INSERT INTO " + tool_type  + " (ID, datetime, coffee, water, brew_time, type) VALUES (%s, %s, %s, %s, %s, %s)"
+                        #change the Database ID back to Auto increment and ID_num to NULL
+                        data = (ID_num  ,time.strftime('%Y-%m-%d %H:%M:%S') , coffee_g , water_g , brew_time,args.coffee_type)
+
+                        cursor.execute(insert_stm,data)
+
+                        db.commit()
+
+                        if args.verbose:
+                            print("data inserted")
+
+                    except mysql.connector.Error as e :
+                        print("Error:",e)
+
+                    finally :
+                        coffee_g = 0
+                        water_g = 0
+                        brew_time = 0
+                        if db.is_connected() :
+                            db.close()
+                            cursor.close()
+
+                            if args.verbose:
+                                print("db closed")
 
         except (KeyboardInterrupt, SystemExit):
+            hx.power_down()
             mylcd.lcd_clear()
             mylcd.backlight(0)
             GPIO.cleanup()
